cbf476ce5c97e973c94e46d843c12ae99c3e162d
GretelF <maria@floruss.net>
2014-05-05 15:43:27 +0200

    added __str__ function to most SchemeObjects

--- interpreter/kmp_py/scheme.py
+++ interpreter/kmp_py/scheme.py
@@ -3,8 +3,8 @@
 class schemeObject:
     def __eq__(self, other):
         return self.type == other.type and self.value == other.value
-
-
+    def __repr__(self):
+        return "<%s: %s>" % (self.type, self.value)
 
 class schemeSingleton(schemeObject):
     _instance = None
@@ -13,16 +13,35 @@ class schemeSingleton(schemeObject):
             singleton._instance = super(schemeSingleton, singleton).__new__(singleton, *args, **kwargs)
         return singleton._instance
 
+
 class schemeFalse(schemeSingleton):
     def __init__(self):
         self.type = 'schemeFalse'
+        self.value = 'False'
+
+    def __str__(self):
+        return '#f'
+
 
 class schemeTrue(schemeSingleton):
     def __init__(self):
         self.type = 'schemeTrue'
+        self.value = 'True'
+
+    def __str__(self):
+        return '#t'
+
+
+class schemeNil(schemeSingleton):
+    def __init__(self):
+        self.type = 'schemeNil'
+        self.value = None
+
+    def __str__(self):
+        return '()'
 
 class schemeCons(schemeObject):
-    def __init__(self,car,cdr):
+    def __init__(self, car, cdr):
         self.car = car
         self.cdr = cdr
         self.type = 'schemeCons'
@@ -30,37 +49,60 @@ class schemeCons(schemeObject):
     def __eq__(self,other):
         return self.type == other.type and self.car == other.car and self.cdr == other.cdr
 
+
 class schemeInteger(schemeObject):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.type = 'schemeInteger'
 
+    def __str__(self):
+        return '%s' % self.value
+
+
 class schemeFloat(schemeObject):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.type = 'schemeFloat'
 
+    def __str__(self):
+        return '%s' % self.value
+
+
 class schemeString(schemeObject):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.type = 'schemeString'
 
+    def __str__(self):
+        return '%s' % self.value
+
+
 class schemeSymbol(schemeObject):
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.type = 'schemeSymbol'
 
+    def __str__(self):
+        return '%s' % self.value
+
+
+
+
 class schemeUserDefinedFunction(schemeObject):
-    def __init__(self, arglist, body, env):
+    def __init__(self, name, arglist, body, env):
+        self.name = name
         self.arglist = arglist
         self.body = body
         self.env = env
         self.type = 'schemeUserDefinedFunction'
+        
 
 class schemeBuiltinFunction(schemeObject):
-    def __init__(self,code):
+    def __init__(self, name, code):
         self.code = code
         self.type = 'schemeBuiltinFunction'
+
+
 
 class schemeBuiltinSyntax(schemeObject):
     def __init__(self, code):
--- interpreter/unittests/test_schemeObjects.py
+++ interpreter/unittests/test_schemeObjects.py
@@ -9,13 +9,16 @@ class TestSchemeFalse(TestCase):
     def test_schemeFalse(self):
         first = scheme.schemeFalse()
         second = scheme.schemeFalse()
-        self.assertIs(first,second,'schemeFalse is no singleton')
+        self.assertIs(first, second, 'schemeFalse is no singleton')
 
 class TestSchemeTrue(TestCase):
      def test_schemeTrue(self):
         first = scheme.schemeTrue()
         second = scheme.schemeTrue()
-        self.assertIs(first,second,'schemeTrue is no singleton')
+        self.assertIs(first, second, 'schemeTrue is no singleton')
+        f = scheme.schemeFalse()
+        t = scheme.schemeTrue()
+        self.assertNotEqual(t, f, "schemeTrue and schemeFalse should not be equal!")
 
 class TestSchemeString(TestCase):
     def test_schemeString(self):
@@ -26,29 +29,29 @@ class TestSchemeInteger(TestCase):
     def test_schemeInteger (self):
         x = scheme.schemeInteger(3)
         y = scheme.schemeInteger(3)
-        self.assertEqual(x.value,3,'schemeInteger does not work')
-        self.assertEqual(x,y,'equal schemeIntegers are not equal')
-#       self.assertIs(x,y,'same schemeIntegers are not same')
+        self.assertEqual(x.value, 3, 'schemeInteger does not work')
+        self.assertEqual(x, y, 'equal schemeIntegers are not equal')
+        self.assertIs(x, y, 'same schemeIntegers are not same')
 
 
 class TestSchemeCons(TestCase):
     def test_schemeCons_equal(self):
-        first = scheme.schemeCons('hello','world')
-        second = scheme.schemeCons('1',2)
-        third = scheme.schemeCons('hello','world')
+        first = scheme.schemeCons('hello', 'world')
+        second = scheme.schemeCons('1', 2)
+        third = scheme.schemeCons('hello', 'world')
 
-        self.assertIsNot(first,second,'schemeCons should not be singleton')
-        self.assertNotEqual(first,second,'schemeCons function "__eq__" does not work: not equal schemeCons are equal')
-        self.assertEqual(first,third,'schemeCons function "__eq__" does not work: equal schemeCons are not equal')
+        self.assertIsNot(first, second, 'schemeCons should not be singleton')
+        self.assertNotEqual(first, second, 'schemeCons function "__eq__" does not work: not equal schemeCons are equal')
+        self.assertEqual(first, third, 'schemeCons function "__eq__" does not work: equal schemeCons are not equal')
 
     def test_schemeCons_car_cdr(self):
         innerCar = scheme.schemeString('Hello')
         innerCdr = scheme.schemeInteger(3)
         outerCar = scheme.schemeInteger(3)
 
-        outerCdr = scheme.schemeCons(innerCar,innerCdr)
-        outerCons = scheme.schemeCons(outerCar,outerCdr)
+        outerCdr = scheme.schemeCons(innerCar, innerCdr)
+        outerCons = scheme.schemeCons(outerCar, outerCdr)
 
-        self.assertEqual(outerCons.car.value,3,'schemeCons car does not work')
-        self.assertEqual(outerCons.cdr.car.value,'Hello','nested schemeCons do not work')
-        self.assertEqual(outerCons.cdr.cdr.value,3,'nested schemeCons do not work')
+        self.assertEqual(outerCons.car.value, 3, 'schemeCons car does not work')
+        self.assertEqual(outerCons.cdr.car.value, 'Hello', 'nested schemeCons do not work')
+        self.assertEqual(outerCons.cdr.cdr.value, 3, 'nested schemeCons do not work')
