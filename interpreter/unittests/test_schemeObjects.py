7bb09adf8889b86cb5d2034656786260dcdfd5d0
GretelF <maria@floruss.net>
2014-05-05 14:43:39 +0200

    added some more unittests (schemeString, schemeInteger,schemeCons)

--- interpreter/unittests/test_schemeObjects.py
+++ interpreter/unittests/test_schemeObjects.py
@@ -17,6 +17,20 @@ class TestSchemeTrue(TestCase):
         second = scheme.schemeTrue()
         self.assertIs(first,second,'schemeTrue is no singleton')
 
+class TestSchemeString(TestCase):
+    def test_schemeString(self):
+        string = scheme.schemeString('Hello')
+        self.assertEqual(string.value,'Hello','schemeString does not work')
+
+class TestSchemeInteger(TestCase):
+    def test_schemeInteger (self):
+        x = scheme.schemeInteger(3)
+        y = scheme.schemeInteger(3)
+        self.assertEqual(x.value,3,'schemeInteger does not work')
+        self.assertEqual(x,y,'equal schemeIntegers are not equal')
+#       self.assertIs(x,y,'same schemeIntegers are not same')
+
+
 class TestSchemeCons(TestCase):
     def test_schemeCons_equal(self):
         first = scheme.schemeCons('hello','world')
@@ -27,10 +41,14 @@ class TestSchemeCons(TestCase):
         self.assertNotEqual(first,second,'schemeCons function "__eq__" does not work: not equal schemeCons are equal')
         self.assertEqual(first,third,'schemeCons function "__eq__" does not work: equal schemeCons are not equal')
 
-    def test_schemeCons_car(self):
-        firstString = scheme.schemeString('Hello')
-        secondString = scheme.schemeString('World')
+    def test_schemeCons_car_cdr(self):
+        innerCar = scheme.schemeString('Hello')
+        innerCdr = scheme.schemeInteger(3)
+        outerCar = scheme.schemeInteger(3)
 
-        firstCons = scheme.schemeCons(firstString,secondString)
+        outerCdr = scheme.schemeCons(innerCar,innerCdr)
+        outerCons = scheme.schemeCons(outerCar,outerCdr)
 
-        self.assertEqual(firstCons.car.value,'Hello','schemeCons car does not work')
+        self.assertEqual(outerCons.car.value,3,'schemeCons car does not work')
+        self.assertEqual(outerCons.cdr.car.value,'Hello','nested schemeCons do not work')
+        self.assertEqual(outerCons.cdr.cdr.value,3,'nested schemeCons do not work')
