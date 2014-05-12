179ba757fa70300bf3bb753f1c025814ab23b888
Maria Floruss <maria@floruss.net>
2014-05-12 11:35:15 +0200

    added schemeException.py which contains usefull exceptions.

---
+++ interpreter/kmp_py/schemeExceptions.py
@@ -0,0 +1,12 @@
+
+class EOFException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class InvalidArgumentException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
