1428feb1247db3a6f3cfc6c5cb6c6d8f1236d251
Maria Floruss <maria@floruss.net>
2014-07-16 19:37:58 +0100

    implemented repl.

--- interpreter/kmp_py/main.py
+++ interpreter/kmp_py/main.py
@@ -1,2 +1,27 @@
-from interpreter.kmp_py import reader
+from interpreter.kmp_py import reader, evaluator, scheme, initialize
+
+def repl():
+    initialize.initialize()
+    e = evaluator.SchemeEvaluator()
+    r = reader.SchemeReader()
+
+    while True:
+        print('> ', end='')
+        toRead = input()
+        stream = scheme.SchemeStringStream(toRead)
+        while not stream.isAtEndOfStream():
+            toEval = r.read(stream)
+            toPrint = e.evaluate(toEval)
+            if toPrint.type == 'schemeVoid':
+                continue
+            print(toPrint)
+
+
+
+
+def main():
+    repl()
+
+if __name__ == '__main__':
+    main()
 
